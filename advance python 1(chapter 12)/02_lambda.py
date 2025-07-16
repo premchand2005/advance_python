25b1a1899f4b5e3c1692da87bc6268dba2cdd9fe
Premchand Kumar Sahu <premchandraja2000@gmail.com>
2025-07-16 10:48:19 +0530

    Add files via upload

---
+++ advance python 1(chapter 12)/02_lambda.py
@@ -0,0 +1,8 @@
+func = lambda a: a+5
+square = lambda x : x*x
+sum = lambda a, b, c: a+b+c
+
+x = 3
+print(func(x))
+print(square(x))
+print(sum(x,2,3))
---
+++ advance python 1(chapter 12)/03_map.py
@@ -0,0 +1,11 @@
+def square(num):
+    return num*num
+l = [1,2,3]
+# method 1
+l2 = []
+for item in l:
+    l2.append(square(item))
+print(l)
+
+    # method 2
+print(list(map(square,l)))    
